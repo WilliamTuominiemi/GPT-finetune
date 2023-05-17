c502832f16df1a4767801430ecb9a080280350d4
williamtuominiemi <william.m.tuominiemi@gmail.com>
2023-05-18 01:34:21 +0300

    clean code and add comment

--- app.js
+++ app.js
@@ -1,82 +1,78 @@
-const { Configuration, OpenAIApi } = require("openai");
-const dotenv = require("dotenv");
-dotenv.config();
-const fs = require("fs");
+import { Configuration, OpenAIApi } from 'openai'
+import dotenv from 'dotenv'
+dotenv.config()
+import fs from 'fs'
 
+// Configure OpenAI
 const configuration = new Configuration({
-    organization: "org-xxx",
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+    apiKey: process.env.OPENAI_API_KEY, // https://platform.openai.com/account/api-keys
+})
+const openai = new OpenAIApi(configuration)
 
-const models = async () => {
-    const response = await openai.listModels();
-    return response;
-};
+// Uploads the data.jsonl training file to OpenAI website
+const upload = async () => {
+    const response = await openai.createFile(fs.createReadStream('data.jsonl'), 'fine-tune')
 
-async function upload() {
-    const response = await openai.createFile(
-        fs.createReadStream("joke.jsonl"),
-        "fine-tune"
-    );
-
-    return response.data;
+    return response.data
 }
 
-async function createModel(id) {
+// Trains a model with the use of the uploaded file
+const createModel = async (id) => {
     const response = await openai.createFineTune({
-        training_file: id,
-        model: "davinci",
-    });
+        training_file: id, // the id of the uploaded file
+        model: 'davinci', // sub-model to use for fine-tuning, existing model supported by OpenAI
+    })
 
-    return response.data;
+    return response.data
 }
 
+// Lists all the models on your OpenAI account
+const models = async () => {
+    const response = await openai.listModels()
+    return response
+}
+
+// Create a new model from the training data and log it's id
 const newModel = () => {
     upload().then((file) => {
-        createModel(file.id).then((model) => {
-            console.log(model);
-        });
-    });
-};
-
-async function getRes(model) {
-    const question = "Why did the chicken cross the road?";
-    console.log("\x1b[35m%s\x1b[0m", question);
-    const completion = await openai.createCompletion({
-        model: model,
-        prompt: question,
-        max_tokens: 50,
-        temperature: 0.7,
-        frequency_penalty: 0.2,
-        presence_penalty: 0.2,
-    });
-
-    let result;
-    for (let i = 0; i < completion.data.choices.length; i++) {
-        result += completion.data.choices[i].text + "\n";
-    }
-    return result;
+        createModel(file.id).then((model) => console.log(model)) // Save the ID somewhere for later use
+    })
 }
 
-async function getModel(id) {
-    const response = await openai.retrieveFineTune(id);
+const test = async (model) => {
+    const prompt = 'Is there any way to get a discount?' // Same as typing it into ChatGPT site
 
-    return response.data;
+    try {
+        const completion = await openai.createCompletion({
+            model: model,
+            prompt: prompt,
+            max_tokens: 50, // The maximum number of tokens to return
+            temperature: 0.7, // 0 - 1 indicating the randomness of the completion
+            frequency_penalty: 0.2, // 0 - 1 indicating the degree to which the model should avoid highly-used words
+            presence_penalty: 0.2, // 0 - 1 indicating the degree to which the model should avoid words that don't appear in the prompt
+        })
+
+        let result = ''
+
+        for (let i = 0; i < completion.data.choices.length; i++) {
+            result += completion.data.choices[i].text + '\n'
+        }
+
+        console.log(result)
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 const run = () => {
     models().then((res) => {
-        // console.log(res.data.data[res.data.data.length - 1]);
-        getRes(res.data.data[res.data.data.length - 1].id).then((res) => {
-            console.log(res);
-            fs.writeFile("result.jsonl", JSON.stringify({ res }), (err) => {
-                if (err) throw err;
-            });
-        });
-    });
-};
+        // The id of the newest model
+        const model_id = res.data.data[res.data.data.length - 1].id
+        test(model_id).then((res) => {
+            console.log(res)
+        })
+    })
+}
 
-// newModel();
-
-run();
+// newModel() // Create model
+run() // Test model
